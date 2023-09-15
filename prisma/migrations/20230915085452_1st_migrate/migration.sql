4894b6b9b3c94e128b2e763808fff0a2f7fab8aa
wintunmmocc <wintun.mmocc@gmail.com>
2023-09-15 15:26:06 +0630

    prisma set up

---
+++ prisma/migrations/20230915085452_1st_migrate/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "profile_url" TEXT,
+    "role" INTEGER DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
